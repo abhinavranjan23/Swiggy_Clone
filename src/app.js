bdc0253d0764155354eb5a1c99a41854d55e70c7
Abhinav Ranjan <abhinavranjan2202@gmail.com>
2024-12-11 04:50:36 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -32,11 +32,11 @@ const Applayout = () => {
 };
 const appRouter = createBrowserRouter([
   {
-    path: "/Swiggy_Clone",
+    path: "/",
     element: <Applayout />,
     children: [
       {
-        path: "/Swiggy_Clone",
+        path: "/",
         element: <Body />,
       },
       {
@@ -56,4 +56,4 @@ const appRouter = createBrowserRouter([
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter} basename='/Swiggy_Clone' />);
+root.render(<RouterProvider router={appRouter} />);
